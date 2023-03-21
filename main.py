e8096d0e665b2537ec8530b047df4e1170d2b6de
Gregory <kgn.0@ya.ru>
2023-03-21 12:08:41 +0300

    some fixes

--- main.py
+++ main.py
@@ -54,14 +54,16 @@ def visualize(graph):
     #     node: graph.nodes[node]['name'] for node in graph.nodes()}, font_size=10)
     plt.axis('off')
     end_time = datetime.now()
-    print(f"Visualized for {(end_time - start_time).total_seconds()} seconds")
+    print(
+        f"Was visualized in {(end_time - start_time).total_seconds()} seconds")
 
     saveDump('visualizedGraph', graph)
 
     plt.show()
 
 
-def build_graph(persons):
+def build_graph(graph, persons):
+    start_time = datetime.now()
     for classmate in group_list:
         classmate_id = classmate["id"]
         classmate_name = classmate["name"]
@@ -80,32 +82,44 @@ def build_graph(persons):
                 graph.add_edge(personId, friendId)
     saveDump('buildedGraph', graph)
 
+    end_time = datetime.now()
+    print(
+        f"Graph was built in {(end_time - start_time).total_seconds()} seconds. {graph}")
+
+    return graph
+
 
 def remove_alone_friends(graph):
-    to_remove = [node for node in graph.nodes() if graph.degree[node] == 1]
+    to_remove = [node for node in graph.nodes() if graph.degree[node] < 2]
     graph.remove_nodes_from(to_remove)
+
+    print('After deleting with alone friends', graph)
+
+    return graph
 
 
 addedPersons = set()
-graph = nx.Graph()
 
 
 def saveDump(file_name, variable):
-    with open(f'{file_name}.picle', 'wb') as f:
+    with open(f'{file_name}.pickle', 'wb') as f:
         pickle.dump(variable, f)
 
 
 def loadDump(file_name):
     try:
         with open(f'{file_name}.pickle', 'rb') as f:
-            print(f'Loading {file_name} from dump')
-            return pickle.load(f)
+            response = pickle.load(f)
+            print(f'Loading {file_name} from dump. Length: {len(response)}')
+
+            return response
     except:
-        return {}
+        return nx.Graph()
 
 
-async def parseFriends(persons, classmate_id, classmate_name, depth=FRIENDS_DEPTH):
-    print('Parsing', classmate_id)
+async def parseFriends(persons, classmate_id, classmate_name, main_friend_id, depth=FRIENDS_DEPTH):
+    # print(
+    #     f'Parsing friends of{classmate_id}. Friend of {main_friend_id}. Length: {len(persons)}')
     while True:
         try:
             friendFriends = await get_friends(classmate_id)
@@ -114,7 +128,7 @@ async def parseFriends(persons, classmate_id, classmate_name, depth=FRIENDS_DEPT
             if depth > 1:
                 for friend_id in friendFriends:
                     if friend_id not in persons:
-                        await parseFriends(persons, friend_id, "", depth=depth-1)
+                        await parseFriends(persons, friend_id, "", main_friend_id, depth-1)
 
             break
         except VkApiException as e:
@@ -135,9 +149,10 @@ async def parse():
         classmate_id = classmate["id"]
         classmate_name = classmate["name"]
 
-        await parseFriends(persons, classmate_id, classmate_name)
+        persons = await parseFriends(persons, classmate_id, classmate_name, classmate_id)
     end_time = datetime.now()
-    print(f"Parsed for {(end_time - start_time).total_seconds()} seconds")
+    print(
+        f"Users was parsed in {(end_time - start_time).total_seconds()} seconds")
 
     return persons
 
@@ -151,8 +166,8 @@ async def main():
         saveDump('persons', persons)
 
     if (len(graph) == 0):
-        build_graph(persons)
-        remove_alone_friends(graph)
+        graph = build_graph(graph, persons)
+        graph = remove_alone_friends(graph)
 
     visualize(graph)
 
